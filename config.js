db5a956f712d9e50a0319d486a23742b46c86179
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-04-16 16:48:02 +0200

    First google validation

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID':'170887897755-mh1o7jas1rhp2g4f6sgnhfrogr5hf1u4.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET':'2K2FhREWDEYgYDLpVuIAJbiJ',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
+
